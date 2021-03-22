d8afe364d33e5efc7f4e393df8918ac1bad643b3
lasseb <lbenninga@schubergphilis.com>
2021-03-22 15:06:57 +0100

    Output service name for connecting from endpoint

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "private_dns_name" {
   value       = aws_vpc_endpoint_service.default.private_dns_name
   description = "Name of the private DNS to access the service"
 }
+
+output "service_name" {
+  value       = aws_vpc_endpoint_service.default.service_name
+  description = "Service name of the Endpoint Service"
+}
